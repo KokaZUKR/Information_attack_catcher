3fe796d6bdc976ed1ba133211bc8576783fb6360
nazariinyzhnyk <nazar.nyzhnyk@gmail.com>
2018-03-22 22:08:40 +0200

    init

--- code/parse_pravda.py
+++ code/parse_pravda.py
@@ -1,3 +1,70 @@
-def parse_pravda(keyword, save_big_csv=1, save_small_csv=1, plot=1):
-    print('im here')
+import pandas as pd
+import requests
+from lxml import html
+
+from code.usr_functions import *
+
+
+def parse_pravda(keyword, save_big_csv=1, save_small_csv=1, save_all_and_plot=1):
+    titles = []
+    dates = []
+    now, to = get_dates()
+    curr_date = now
+
+    #  REWRITE THIS LOOP INTO USR_FUNCTIONS!!!!
+    #  REWRITE FROM LXML TO BEAUTIFUL SOUP!!!!!
+
+    i = 1
+    while curr_date > to:
+        page = requests.get(
+            'https://www.pravda.com.ua/search/page_' + str(i) + '/?search=' + str(cyrillic_url(keyword)))
+        tree = html.fromstring(page.content)
+        ptitles = tree.xpath('//div[@class="article__title"]/a/text()')[:30]
+        pdates = tree.xpath('//div[@class="article article_search"]/div[@class="article__date"]/text()')[:30]
+        pdates = [x[:10] for x in pdates]
+        pdates = [datetime.datetime.strptime(date, '%d.%m.%Y').date() for date in pdates]
+        titles.append(ptitles)
+        dates.append(pdates)
+        curr_date = pdates[29]
+        i += 1
+
+    dates = unlist_nested(dates)
+    titles = unlist_nested(titles)
+
+    dates = leave_list(dates, to)
+    titles = titles[:len(dates)]
+
+    fullDF = pd.DataFrame(
+        {'title': titles,
+         'date': dates,
+         })
+
+    if save_big_csv == 1:
+        fullDF.to_csv('saved_data/pravda_big_df.csv', index=False)
+
+    if save_small_csv == 1:
+        lightDF = fullDF.groupby('date').count()
+        lightDF.to_csv('saved_data/pravda_light_df.csv')
+
+    if save_all_and_plot == 1:
+        dftom = pd.read_csv('saved_data/pravda_light_df.csv')
+        alldays = []
+        step = datetime.timedelta(days=1)
+        while to <= now:
+            alldays.append(to)
+            to += step
+        toVis = pd.DataFrame(
+            {'date': alldays,
+             })
+
+        toVis['date'] = toVis['date'].astype('datetime64[ns]')
+        dftom['date'] = dftom['date'].astype('datetime64[ns]')
+        dftom['title'] = dftom['title'].astype('int')
+        toVis = toVis.merge(dftom, how='left')
+        toVis = toVis.fillna(0)
+        toVis.to_csv('saved_data/pravda_light_df_all_dates.csv', index=False)
+
+        plot_count_day(alldays, toVis['title'])
+        print('https://www.pravda.com.ua WAS PARSED')
+
     return 0
--- code/usr_functions.py
+++ code/usr_functions.py
@@ -1,5 +1,41 @@
+import datetime
 from urllib import request
+
+import datedelta
+import matplotlib.pyplot as plt
 
 
 def cyrillic_url(cyr_str):
     return request.quote(cyr_str.encode('cp1251'))
+
+
+def leave_list(lst, cond):
+    to_append = []
+    for i in range(len(lst)):
+        if lst[i] > cond:
+            to_append.append(lst[i])
+    return to_append
+
+
+def get_dates(month_to_parse=3):
+    now = (datetime.datetime.now())
+    to = (now - month_to_parse * datedelta.MONTH)
+    to = to.date()
+    now = now.date()
+    return now, to
+
+
+def unlist_nested(lst):
+    return [item for sublist in lst for item in sublist]
+
+
+def plot_count_day(period, count_news):
+    plt.figure(num=None, figsize=(15, 8))
+    plt.plot(period, count_news)
+    plt.xticks(period, [str(i) for i in period], rotation='vertical')
+    plt.show()
+
+    # plt.figure(num=None, figsize=(15, 8))
+    # plt.plot(alldays, toVis['title'])
+    # plt.xticks(alldays, [str(i) for i in alldays], rotation='vertical')
+    # plt.show()
