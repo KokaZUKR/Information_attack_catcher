acc142a4b3988699dad7d483035b869ad3b972f1
nazariinyzhnyk <nazar.nyzhnyk@gmail.com>
2018-03-22 20:49:19 +0200

    init

---
+++ code/main.py
@@ -0,0 +1,10 @@
+import warnings
+
+from code.parse_pravda import parse_pravda
+
+warnings.filterwarnings("ignore")
+
+STRING_TO_SEARCH = 'нато'  # to be more 'user-friendly': just type your word here
+
+if __name__ == '__main__':
+    parse_pravda(STRING_TO_SEARCH)
---
+++ code/parse_pravda.py
@@ -0,0 +1,3 @@
+def parse_pravda(keyword, save_big_csv=1, save_small_csv=1, plot=1):
+    print('im here')
+    return 0
