2be8a41b3ca61cd96c975cdf07e59aa1dbcd1b23
nazariinyzhnyk <nazar.nyzhnyk@gmail.com>
2018-03-29 14:02:40 +0300

    real-time update fixed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # NATO Science for Peace and Security project: CyRADARS
-information attack unit
+Information attack unit.
 This project was designed to parse web sources in real time and predict different informational attacks.
 prof. Lande and prof. Chertov tasks.
--- code/real_time_update.py
+++ code/real_time_update.py
@@ -14,28 +14,29 @@ minutes_to_parse = 10  # how much time to spend parsing site
 reload_sec = 5  # reload every ... seconds
 n_iterations = int(minutes_to_parse * 60 / reload_sec)
 
-for i in range(1, n_iterations):
+for i in range(n_iterations):
     page = requests.get('https://www.pravda.com.ua/news/')
     tree = html.fromstring(page.content)
     titles_parsed = tree.xpath('//div[@class="block block_news_all"]//div[@class="article__title"]/a/text()')
-    new_titles = new_news(titles_parsed, tit)
-    print(new_titles)
+    print('===========================')
+    print('Parsed on {} sec:'.format(reload_sec * i))
 
-    if not new_titles:
-        print('-------------------')
-        print('Parsed on {} sec'.format(i * reload_sec))
-        print('NOTHING')
-        print('-------------------')
-
+    if i == 0:
+        tit = titles_parsed
+        print(tit)
     else:
-        for i in range(len(new_titles) - 1, -1, -1):
-            tit.append(new_titles[i])  # insert(0, new_titles[i])
-        # tit = unlist_nested(tit)
-        print('-------------------')
-        print('Parsed on {} sec'.format(i * reload_sec))
-        print(new_titles)
-        print('-------------------')
+        new_titles = new_news(titles_parsed, tit)
+        if not new_titles:
+            print('\nNo new titles found.')
+        else:
+            print('New titles:')
+            print(new_titles)
+            for new_title in new_titles:
+                tit.insert(0, new_title)
         del new_titles[:]
+        print('\nAll titles:')
+        print(tit)
+    print('===========================')
 
     time.sleep(reload_sec)
 
